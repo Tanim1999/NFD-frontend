7a5be9026a1b37747183ff5cce5f3f08dcf25044
Tanim1999 <ihtanim56@gmail.com>
2024-01-13 20:08:54 +0600

    completed navbar

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../sections/Navbar";
+
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/routes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className=' bg-[#E8EBF1] max-w-screen-xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
+
+)
